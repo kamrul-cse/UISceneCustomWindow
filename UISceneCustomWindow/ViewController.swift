3735d67a58269bcaa2590525fcd91ba674fd0317
Md. Kamrul Hasan <mhgolap11@gmail.com>
2020-05-23 14:37:53 +0600

    Initial Commit

---
+++ UISceneCustomWindow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISceneCustomWindow
+//
+//  Created by Md. Kamrul Hasan on 23/5/20.
+//  Copyright © 2020 MKHG Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
